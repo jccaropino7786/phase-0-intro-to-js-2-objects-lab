be84265270615e87b42e36b022ebbc3b268fccc8
john Caropino <jccaropino7786@live.com>
2022-11-23 08:40:56 -0800

    commit tree 46cf37049a6c4ef0f90c3d819e93ad3d299032a1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = {
+    name: "JC",
+    streetAddress:"1234 Candyland Dr."
+}
+
+function updateEmployeeWithKeyAndValue(){
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+
+}
+
+function deleteFromEmployeeByKey90(){
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+
+}
